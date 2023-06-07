a6bc0737b157ad62aa5ccfd9ba76e88fae039e7c
byeong <qudcks4885@gmail.com>
2023-06-07 18:36:40 +0900

    영화,감독,장르명 인덱스 생성

--- db_movie.py
+++ db_movie.py
@@ -5,93 +5,106 @@ conn = pymysql.connect(host='localhost', user='root', password='livinla98',db='m
 cursor = conn.cursor(pymysql.cursors.DictCursor)
 
 
-df = pd.read_excel('./movie_list.xls')
+# df = pd.read_excel('./movie_list.xls')
 
-df = df.fillna('N/A')
+# df = df.fillna('N/A')
 
-for idx, row in df.iterrows() :
-    # 영화 테이블
-    m_name = row['영화명']
-    m_ename= row['영화명(영문)']
+# for idx, row in df.iterrows() :
+#     # 영화 테이블
+#     m_name = row['영화명']
+#     m_ename= row['영화명(영문)']
 
-    m_year = row['제작연도']
-    if m_year=='N/A' : m_year = None
+#     m_year = row['제작연도']
+#     if m_year=='N/A' : m_year = None
 
-    m_country = row['제작국가']
-    type = row['유형']
-    state = row['제작상태']
-    company = row['제작사']
+#     m_country = row['제작국가']
+#     type = row['유형']
+#     state = row['제작상태']
+#     company = row['제작사']
 
-    sql = '''insert into movie (m_name,m_ename,m_year,m_country,type,state,company)
-    values (%s,%s,%s,%s,%s,%s,%s)'''
+#     sql = '''insert into movie (m_name,m_ename,m_year,m_country,type,state,company)
+#     values (%s,%s,%s,%s,%s,%s,%s)'''
 
 
-    val = (m_name,m_ename,m_year,m_country,type,state,company)
-    cursor.execute(sql,val)
+#     val = (m_name,m_ename,m_year,m_country,type,state,company)
+#     cursor.execute(sql,val)
 
-    m_id = cursor.lastrowid
+#     m_id = cursor.lastrowid
     
-    # sql = "select m_id from movie where m_name = %s"
-    # val = (m_name,)
-    # mm_id = cursor.execute(sql,val)
-    # print(mm_id)
+#     # sql = "select m_id from movie where m_name = %s"
+#     # val = (m_name,)
+#     # mm_id = cursor.execute(sql,val)
+#     # print(mm_id)
 
 
-    # 감독 테이블 , 중간테이블
-    d_names = row['감독'].split(',')
-    for d_name in d_names :
-        d_name = d_name.strip()
-        if d_name != 'N/A' : 
-            sql = "SELECT d_id from director where d_name = %s"
-            val = (d_name,)
-            cursor.execute(sql,val)
-            res = cursor.fetchone()
+#     # 감독 테이블 , 중간테이블
+#     d_names = row['감독'].split(',')
+#     for d_name in d_names :
+#         d_name = d_name.strip()
+#         if d_name != 'N/A' : 
+#             sql = "SELECT d_id from director where d_name = %s"
+#             val = (d_name,)
+#             cursor.execute(sql,val)
+#             res = cursor.fetchone()
 
-            #감독 아이디가 없는 경우
-            if res is None : 
-                sql = "INSERT INTO director (d_name) values (%s)"
-                val = (d_name,)
-                cursor.execute(sql,val)
+#             #감독 아이디가 없는 경우
+#             if res is None : 
+#                 sql = "INSERT INTO director (d_name) values (%s)"
+#                 val = (d_name,)
+#                 cursor.execute(sql,val)
 
-                d_id = cursor.lastrowid
+#                 d_id = cursor.lastrowid
 
 
-                sql = f"Select * from mdinter where m_id = {m_id} and d_id ={d_id}"
-                cursor.execute(sql)
+#                 sql = f"Select * from mdinter where m_id = {m_id} and d_id ={d_id}"
+#                 cursor.execute(sql)
 
-                if cursor.fetchone() is None :
-                    sql = "INSERT INTO mdinter (m_id,d_id) values (%s,%s)"
-                    val =(m_id,d_id)
-                    cursor.execute(sql,val)
-                else :
-                   print(m_name , d_name , m_id , d_id)
-            # 감독 아이디가 이미 존재하는 경우
-            else :
-               # print(f"이미 존재하는 감독 : {d_name} , {res['d_id']}")
-                sql = f"Select * from mdinter where m_id = {m_id} and d_id ={res['d_id']}"
-                cursor.execute(sql)
-                if cursor.fetchone() is None :
-                    sql = "INSERT INTO mdinter (m_id,d_id) values (%s,%s)"
-                    val = (m_id,res['d_id'])
-                    cursor.execute(sql,val)
-                else :
-                    print(m_name , d_name , m_id , d_id)
+#                 if cursor.fetchone() is None :
+#                     sql = "INSERT INTO mdinter (m_id,d_id) values (%s,%s)"
+#                     val =(m_id,d_id)
+#                     cursor.execute(sql,val)
+#                 else :
+#                    print(m_name , d_name , m_id , d_id)
+#             # 감독 아이디가 이미 존재하는 경우
+#             else :
+#                # print(f"이미 존재하는 감독 : {d_name} , {res['d_id']}")
+#                 sql = f"Select * from mdinter where m_id = {m_id} and d_id ={res['d_id']}"
+#                 cursor.execute(sql)
+#                 if cursor.fetchone() is None :
+#                     sql = "INSERT INTO mdinter (m_id,d_id) values (%s,%s)"
+#                     val = (m_id,res['d_id'])
+#                     cursor.execute(sql,val)
+#                 else :
+#                     print(m_name , d_name , m_id , d_id)
             
             
-    #mdinter 중복 키 들어올 시 예외처리
+#     #mdinter 중복 키 들어올 시 예외처리
 
-    #장르 테이블
-    g_names = row['장르'].split(',')
-    for g_name in g_names :
-        g_name = g_name.strip()
+#     #장르 테이블
+#     g_names = row['장르'].split(',')
+#     for g_name in g_names :
+#         g_name = g_name.strip()
 
-        sql = "INSERT INTO genre (m_id,g_name) values (%s,%s)"
-        val = (m_id,g_name)
-        cursor.execute(sql,val)
+#         sql = "INSERT INTO genre (m_id,g_name) values (%s,%s)"
+#         val = (m_id,g_name)
+#         cursor.execute(sql,val)
 
-    if (idx+1) % 10000 == 0 :
-        print(f"-----------{idx+1} movies updated--------------")
+#     if (idx+1) % 10000 == 0 :
+#         print(f"-----------{idx+1} movies updated--------------")
 
 # conn.commit()
 
+# conn.close()
+
+
+# 인덱스 생성
+
+conn = pymysql.connect(host='localhost', user='root', password='livinla98',db='movie_info', charset='utf8')
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+cursor.execute("CREATE INDEX idx_m_name on movie (m_name)")
+cursor.execute("CREATE INDEX idx_d_name on director (d_name)")
+cursor.execute("CREATE INDEX idx_g_name on genre (g_name)")
+
+conn.commit()
 conn.close()
